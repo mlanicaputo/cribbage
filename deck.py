03f4a59ae3d9f331e4b39aeaf3b3a75be6a67921
Milo Lani-Caputo <mlanicaputo@gmail.com>
2025-03-05 16:58:25 -0500

    Finish Rank, Suit, Color, Card, Deck classes

--- deck.py
+++ deck.py
@@ -1,9 +1,16 @@
+"""Contains the following classes:
+Rank, Suit, Color, Card, Deck.
+"""
+
 from enum import Enum
 import random
 from typing import List
 
 
 class Rank(Enum):
+    """A card rank. E.g. ACE, 2, 9, KING.
+    """
+
     ACE = 1
     R_2 = 2
     R_3 = 3
@@ -14,19 +21,26 @@ class Rank(Enum):
     R_8 = 8
     R_9 = 9
     R_10 = 10
-    JACK = 10
-    QUEEN = 10
-    KING = 10
+    JACK = 11
+    QUEEN = 12
+    KING = 13
 
 
 class Suit(Enum):
-    CLUBS = 1
-    DIAMONDS = 2
-    HEARTS = 3
-    SPADES = 4
+    """A card's suit. E.g. CLUBS, SPADES.
+    """
+
+    CLUBS = 0
+    DIAMONDS = 1
+    HEARTS = 2
+    SPADES = 3
 
 
 class Color(Enum):
+    """A color. E.g. RED, GREEN.
+    """
+
+    BLACK = 0
     RED = 1
     GREEN = 2
     BLUE = 3
@@ -35,18 +49,65 @@ class Color(Enum):
 class Card:
     rank: Rank
     suit: Suit
+    value: int
+    color: Color
 
-    def __init__(self, rank, suit):
-        self.rank = rank
-        self.suit = suit
+    def __init__(self, rank: Rank, suit: Suit) -> None:
+        self._rank = rank
+        self._suit = suit
+
+        self._color = Color.BLACK if suit in (Suit.CLUBS, Suit.SPADES) else Color.RED
+
+        self._value = rank.value if rank.value <= 10 else 10
+    
 
     @property
-    def rank(self):
+    def rank(self) -> Rank:
+        """Return the card's rank. E.g. Rank.ACE, Rank.R_2.
+
+        Args:
+            None
+        Returns:
+            Rank
+        """
+
         return self._rank
 
     @property
-    def suit(self):
+    def suit(self) -> Suit:
+        """Return the card's suit. E.g. Suit.SPADES.
+
+        Args:
+            None
+        Returns:
+            Suit
+        """
+
         return self._suit
+
+    @property
+    def value(self) -> int:
+        """Return the card's value. E.g. 1, 4, 10.
+
+        Args:
+            None
+        Returns:
+            Rank
+        """
+
+        return self._value
+
+    @property
+    def color(self) -> Color:
+        """Return the card's color. E.g. Color.RED.
+
+        Args:
+            None
+        Returns:
+            Color
+        """
+
+        return self._color
 
 
 class Deck:
@@ -57,16 +118,31 @@ class Deck:
     """
     cards: List[Card]
 
-    def __init__(self):
-        self.cards = list()
+    def __init__(self, shuffle=True) -> None:
+        self._cards = list()
 
         for suit in Suit:
             for rank in Rank:
-                cards.append(Card(rank, suit))
+                self._cards.append(Card(rank, suit))
 
-    def shuffle(self):
+        if shuffle:
+            self.shuffle()
+
+    @property
+    def cards(self):
+        """Return cards in the deck.
+        """
+
+        return self._cards
+
+    def shuffle(self) -> None:
+        """Shuffle the deck, reordering cards randomly.
+        """
+
         random.shuffle(self.cards)
 
     def draw(self) -> Card:
-        # draw a card from the top of the deck
+        """Draw a card from the top of the deck.
+        """
+
         return self.cards.pop()
